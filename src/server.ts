c11df09158d108ea187d9321ced667ae6e3f2f24
WilliamFilh0 <williamfilho8908@gmail.com>
2023-09-17 14:28:26 -0300

    Primeira rota criada

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import { fastify} from 'fastify'
+import  {prisma} from './lib/prisma'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello World'
+
+app.get('/prompts', async () => {
+  const prompts = await prisma.prompt.findMany()
+  
+  return prompts
 })
+/*
+app.get('/', () => {
+  return 'Hello world'
+})*/
 
 app.listen({
-  port: 3333,
-}).then(() => {
+  port: 3330,
+}).then(() => { 
   console.log('HTTP Server Running!')
 })
+
