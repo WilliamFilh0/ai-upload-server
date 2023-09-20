771944a03df0884c3f2751f121b1c33c9a4f7162
WilliamFilh0 <williamfilho8908@gmail.com>
2023-09-20 10:18:42 -0300

    Criando a rota transcription

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -1,32 +1,32 @@
 import { FastifyInstance } from "fastify";
-import { fastifyMultipart } from  "@fastify/multipart"
+import { fastifyMultipart } from "@fastify/multipart"
 import path from "node:path";
 import { randomUUID } from 'node:crypto'
 import fs from 'node:fs'
 import { pipeline } from "node:stream";
 import { promisify } from "node:util";
-import {prisma} from "../lib/prisma"
+import { prisma } from "../lib/prisma"
 
 const pump = promisify(pipeline)
 
-export async function uploadVideoRoute(app: FastifyInstance){
+export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, {
-    limits:{
-      fileSize:1_048_576 * 25, //25mb
+    limits: {
+      fileSize: 1_048_576 * 25, //25mb
     }
   })
 
   app.post('/videos', async (request, reply) => {
     const data = await request.file()
 
-    if(!data){
-      return reply.status(488).send({error: 'Missing file input.'})
+    if (!data) {
+      return reply.status(488).send({ error: 'Missing file input.' })
     }
 
     const extension = path.extname(data.filename)
 
-    if(extension !== '.mp3'){
-      return reply.status(488).send({error: 'Invalid inpu type, please upload a MP3.'})
+    if (extension !== '.mp3') {
+      return reply.status(488).send({ error: 'Invalid inpu type, please upload a MP3.' })
     }
 
     const fileBaseName = path.basename(data.filename, extension)
@@ -36,9 +36,9 @@ export async function uploadVideoRoute(app: FastifyInstance){
     await pump(data.file, fs.createWriteStream(uploadDestination))
 
     const video = await prisma.vide.create({
-      data:{
+      data: {
         name: data.filename,
-        path:uploadDestination,
+        path: uploadDestination,
       }
     })
 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import { fastify} from 'fastify'
+import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3330,
-}).then(() => { 
+}).then(() => {
   console.log('HTTP Server Running!')
 })
 
